3e366269540a3aebb36d5371f5813e8ae2daf660
Yuri Skurikhin <yunixon@gmail.com>
2021-08-27 14:43:50 +0300

    Fix private url with colons in filename

--- lib/s3_relay/private_url.rb
+++ lib/s3_relay/private_url.rb
@@ -4,7 +4,13 @@ module S3Relay
     attr_reader :expires, :path
 
     def initialize(uuid, file, options={})
-      filename = Addressable::URI.escape(file).gsub("+", "%2B").gsub("#", "%23").gsub("?", "%3F").gsub("\\", "%5C")
+      filename =
+        if file.include?(":")
+          filename.gsub(":", "%3A")
+        else
+          Addressable::URI.escape(file)
+        end
+      filename = filename.gsub("+", "%2B").gsub("#", "%23").gsub("?", "%3F").gsub("\\", "%5C")
       @path    = [uuid, filename].join("/")
       @expires = (options[:expires] || 10.minutes.from_now).to_i
     end
